85e94369ff16a8d4752c7c7e4ac2f6bd43346d3c
Brianna Gaines <briannagaines31@gmail.com>
2022-12-07 14:45:57 -0800

    adds index functionality

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,8 +1,20 @@
 
-export default function Index({cards, card}){
-  return (  
-    cards.map((individualCard, idx) =>{
-      return <h1>{individualCard.term}</h1>
-      console.log("individual card: ",individualCard.term)
-    }))
+export default function Index({cards, setCards, setAddCard,card,setCard,newCard, setNewCard}){
+  
+  return (  <>
+   {/* show all cards and known cards have a green outline, unknown have red outline 
+    try to use pagination or something similar to move left and right between cards? */}
+    <button onClick={()=> setNewCard(true)}>Add a New Card</button>
+  
+   { cards.map((individualCard) =>{
+    let border = "solid red"
+    if( individualCard.known == true){
+        border = "solid green"
+    }
+      return <div style={{border: border}}>
+        <h1 key={individualCard._id}>{individualCard.term}</h1>
+        <h6>{individualCard.collection}</h6>
+      </div> 
+    })}
+    </>)
 }
