ba2ca36224c62fe96e9def50d05e4eba5cef5822
Brianna Gaines <briannagaines31@gmail.com>
2022-12-07 09:11:53 -0800

    creates main.go file

--- server/main.go
+++ server/main.go
@@ -4,4 +4,11 @@ import(
 	"fmt"
 	"log"
 	"net/http"
+	"github.com/briannag31/mv-project3-quiz/router"
 )
+
+func main (){
+	r := router.Router()
+	fmt.Println("starting the server on port 3001")
+	log.Fatal(http.ListenAndServe(":3001", r))
+}
